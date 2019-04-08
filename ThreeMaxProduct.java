a9203eaa1ab812ca952b691755f4d6ef00aeb447
Arth Shah <arthshah94@gmail.com>
2019-04-08 00:55:42 -0500

    Maximum Product of Three Numbers

---
+++ ThreeMaxProduct.java
@@ -0,0 +1,9 @@
+class ThreeMaxProduct
+{
+    public int maximumProduct(int[] nums)
+	{
+        int len = nums.length;
+		Arrays.sort(nums);
+        return Math.max(nums[0] * nums[1] * nums[len - 1], nums[len - 3] * nums[len - 2] * nums[len - 1]);
+    }
+}
